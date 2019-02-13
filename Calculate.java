cd1375449478d54af407f6c7352873892698ee7e
nemecleha <nemecleha@bk.ru>
2019-02-13 19:12:13 +0300

    user imnput

--- Calculate.java
+++ Calculate.java
@@ -1,4 +1,8 @@
  public class Calculate {
  public static void main(String[]arg){ 
-System.out.println("Hello world!!");}
+System.out.println("Hello world!!");
+  int first =Integer.valueOf(arg[0]);
+ int second = Integer.valueOf(arg[1]);
+ int summ = first + second; 
+System.out.println (summ);}
 }
